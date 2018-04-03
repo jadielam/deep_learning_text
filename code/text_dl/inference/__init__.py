320cbd9d67929578a7c8e099ba3347f82d62ff44
Jadiel deArmas <Jadiel.X.De.Armas.-ND@disney.com>
2018-04-02 21:13:36 -0400

    Added generator for multiple fields

--- code/text_dl/inference/__init__.py
+++ code/text_dl/inference/__init__.py
@@ -1,8 +1,10 @@
 from text_dl.common.factories import generic_factory
 from .classification import fields_generator as class_fields_generator
+from .classification import extra_vocabs_fields_generator
 
 FIELDS_D = {
-    "class": class_fields_generator
+    "class": class_fields_generator,
+    "class_intent": extra_vocabs_fields_generator
 }
 
 fields_factory = generic_factory(FIELDS_D, "fields")
--- code/text_dl/inference/classification.py
+++ code/text_dl/inference/classification.py
@@ -1,6 +1,31 @@
 import torch
 import torchtext.data as data
 
+def extra_vocabs_fields_generator(training_path, batch_size = 1, 
+                                    validation_path = None, 
+                                    vocav_type = "glove.twitter.27B.200d", 
+                                    entities_annotations = ['B-PARK', 'I-PARK', 'B-ATTR', 'I-ATTR', 'B-REST', 'I-REST', 'B-RESO', 'I-RESO', 'B-PLAC', 'I-PLAC', 'B-CHAR', 'I-CHAR', 'B-ENTE', 'I-ENTE']):
+    text_field = data.Field(sequential = True, eos_token = '<eos>',
+                            init_token = '<sos>', pad_token = '<pad>',
+                            unk_token = '<unk>')
+    
+    annotations_field = data.Field(sequential = True, eos_token = '<eos>',
+                                    init_token = '<sos>', pad_token = '<pad>',
+                                    unk_token = '<unk>')
+    
+    dataset = data.Dataset(examples = [], fields = [("text", text_field), ('ner', annotations_field)])
+    vocabulary_datasets = [data.TabularDataset(path = a, format = "csv",
+                                                fields = [("text", text_field), ('ner', annotations_field)],
+                                                skip_header = True) if a else None for a in 
+                                                [training_path, validation_path]]
+    text_field.build_vocab(*[a for a in vocabulary_datasets if a], vectors = vocab_type)
+    annotations_field.build_vocab(*[a for a in vocabulary_datasets if a], 
+                                    vectors = OneHotEncoderVectors(entities_annotations))
+
+    text_vocabulary = text_field.vocab
+    ner_vocabulary = annotations_field.vocab
+    return text_vocabulary, ner_vocabulary, ('text', text_field), ('ner', annotations_field), dataset
+
 def fields_generator(training_path, batch_size = 1, validation_path = None, vocab_type = "glove.twitter.27B.200d"):
     text_field = data.Field(sequential = True, eos_token = '<eos>',
                             init_token = '<sos>', pad_token = '<pad>',
